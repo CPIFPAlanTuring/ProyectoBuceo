a27de7e763017e6363f28208f1d9ab8f0ce96ada
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-12-16 00:42:41 +0100

    añadido de fotografias de galeria y estilos, y correccion de estilos boton de leer mas en cursos

--- js/functions.js
+++ js/functions.js
@@ -21,3 +21,20 @@ function sound(element){
 function silence(element){
   document.getElementById(element).pause();
 }
+
+
+/****function autoslideshow***** */
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+} 
