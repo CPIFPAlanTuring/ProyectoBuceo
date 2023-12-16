cd3f9463b1ce3a5b7c282cf9671b2b6bcd1babc9
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-12-16 12:20:36 +0100

    corregido colores en textos varios, agregado controles para galeria, modificacion de font family

--- js/functions.js
+++ js/functions.js
@@ -24,7 +24,7 @@ function silence(element){
 
 
 /****function autoslideshow***** */
-let slideIndex = 0;
+/*let slideIndex = 0;
 showSlides();
 
 function showSlides() {
@@ -37,4 +37,51 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
-} 
+} */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function autoSlide() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(autoSlide, 2000); // Change image every 2 seconds
+}
+
+autoSlide(); // Start automatic slide show
