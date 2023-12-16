46495532c8bf6765be70847c892cb47a6456eb2b
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-12-16 18:48:17 +0100

    añadida la version en ingles de la pagina completa

--- js/functions.js
+++ js/functions.js
@@ -102,7 +102,23 @@ window.onclick = function(event) {
 } 
 
 /*************funciones seccion conservacion marina************* */
-function toggleReadMore() {
+function toggleReadMoreEnglish() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+} 
+
+function toggleReadMoreSpanish() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("mas");
   var btnText = document.getElementById("myBtn");
